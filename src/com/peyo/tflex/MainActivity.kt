bfe72df17b7f479ea377cf671d00af890909847b
Peter Yoon <peyo.hd@gmail.com>
2024-06-18 16:33:42 +0900

    convert to dual

--- src/com/peyo/tflex/MainActivity.kt
+++ src/com/peyo/tflex/MainActivity.kt
@@ -32,118 +32,220 @@ class MainActivity: Activity() {
                 "test_image5.jpg", "test_image.jpg", "test_image1.jpg",
                 "test_image2.jpg", "test_image3.jpg", "test_image4.jpg",
                 "test_image5.jpg")
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
         loadLabels()
-        tfliteModel = FileUtil.loadMappedFile(this, "mobilenet_v1_1_0_224_float.tflite")
+        tfliteModel1 = FileUtil.loadMappedFile(this, "mobilenet_v1_1_0_224_float.tflite")
+        tfliteModel2 = FileUtil.loadMappedFile(this, "mobilenet_v1_1_0_224_quantized.tflite")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stop = true
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        tflite.close()
+        tflite1.close()
     }
 
     fun onComputeClick(v: View) {
-        thread {
-            val options = Interpreter.Options()
-            if (nnapiToggle.isChecked) {
-                options.addDelegate(NnApiDelegate())
-            } else {
-                options.setNumThreads(1)
+        if (running1 || running2) {
+            stop = true
+        } else {
+            stop = false
+            if (!running1) {
+                thread {
+                    running1 = true
+                    val options = Interpreter.Options()
+                    options.setNumThreads(4)
+                    tflite1 = Interpreter(tfliteModel1, options)
+
+                    inferenceTime1 = 0
+                    firstFrame1 = true
+
+                    while (!stop) {
+                        for (image in images) {
+                            convertBitmapToByteBuffer1(getBitmap1(image))
+
+                            startTime1 = SystemClock.uptimeMillis()
+                            tflite1.run(imgData1, outputs1)
+                            printLabels1()
+
+                            if (stop) {
+                                break
+                            }
+                        }
+                    }
+                    tflite1.close()
+                    running1 = false
+                }
             }
-            tflite = Interpreter(tfliteModel, options)
+            if (!running2) {
+                thread {
+                    running2 = true
+                    val options = Interpreter.Options()
+                    options.addDelegate(NnApiDelegate())
+                    tflite2 = Interpreter(tfliteModel2, options)
 
-            inferenceTime = 0
-            firstFrame = true
-            for(image in images) {
-                convertBitmapToByteBuffer(getBitmap(image))
+                    inferenceTime2 = 0
+                    firstFrame2 = true
 
-                startTime = SystemClock.uptimeMillis()
-                tflite.run(imgData, outputs)
-                printLabels()
+                    while (!stop) {
+                        for (image in images) {
+                            convertBitmapToByteBuffer2(getBitmap2(image))
 
-                Thread.sleep(500)
+                            startTime2 = SystemClock.uptimeMillis()
+                            tflite2.run(imgData2, outputs2)
+                            printLabels2()
+
+                            if (stop) {
+                                break
+                            }
+                        }
+                    }
+                    tflite2.close()
+                    running2 = false
+                }
             }
 
-            runOnUiThread {
-                textView1.text = "Summary: \n\t Average Inference time (ms): " +
-                        "${inferenceTime / (images.size - 1)}"
-                textView2.text = ""
-            }
-            tflite.close()
         }
      }
 
-    private fun printLabels() {
-        val runtime = SystemClock.uptimeMillis() - startTime
+    private fun printLabels1() {
+        val runtime = SystemClock.uptimeMillis() - startTime1
 
         for (i in 0 until getNumLabels()) {
-            sortedLabels.add(SimpleEntry(labelList[i], outputs[0][i]))
-            if (sortedLabels.size > RESULTS_TO_SHOW) {
-                sortedLabels.poll()
+            sortedLabels1.add(SimpleEntry(labelList[i], outputs1[0][i]))
+            if (sortedLabels1.size > RESULTS_TO_SHOW) {
+                sortedLabels1.poll()
             }
         }
 
         var text = ""
-        for (i in 0 until sortedLabels.size) {
-            val label = sortedLabels.poll()
+        for (i in 0 until sortedLabels1.size) {
+            val label = sortedLabels1.poll()
             text = String.format("\n  %s: %f", label.key, label.value) + text
         }
         text = "Result:" + text
 
         runOnUiThread {
-            textView1.text = "Inference time (ms): " + runtime
-            if (firstFrame) {
-                firstFrame = false
+            time1.text = "Inference time (ms): " + runtime
+            if (firstFrame1) {
+                firstFrame1 = false
             } else {
-                inferenceTime += runtime
+                inferenceTime1 += runtime
             }
 
-            textView2.text = text
+            result1.text = text
         }
     }
 
-    private var startTime: Long = 0
-    private var inferenceTime : Long = 0
-    private var firstFrame : Boolean = true
+    private fun printLabels2() {
+        if ((count2 % 8) != 0L) return
+        val runtime = SystemClock.uptimeMillis() - startTime2
 
-    private lateinit var tfliteModel: MappedByteBuffer
-    private var imgData: ByteBuffer? = null
-    private val intValues = IntArray(224 * 224)
-    private val IMAGE_MEAN = 128.0f
-    private val IMAGE_STD = 128.0f
-
-    private fun convertBitmapToByteBuffer(bitmap: Bitmap) {
-        if (imgData == null) {
-            imgData = ByteBuffer.allocateDirect(
-                    1 * 224 * 224 * 3 * 4)
-            imgData!!.order(ByteOrder.nativeOrder())
+        for (i in 0 until getNumLabels()) {
+            sortedLabels2.add(SimpleEntry(labelList[i], outputs2[0][i]))
+            if (sortedLabels2.size > RESULTS_TO_SHOW) {
+                sortedLabels2.poll()
+            }
         }
-        imgData!!.rewind()
+
+        var text = ""
+        for (i in 0 until sortedLabels2.size) {
+            val label = sortedLabels2.poll()
+            text = String.format("\n  %s: %d", label.key, label.value) + text
+        }
+        text = "Result:" + text
+
+        runOnUiThread {
+            time2.text = "Inference time (ms): " + runtime
+            if (firstFrame2) {
+                firstFrame2 = false
+            } else {
+                inferenceTime2 += runtime
+            }
+
+            result2.text = text
+        }
+    }
+
+    private var running1 : Boolean = false
+    private var running2 : Boolean = false
+    private var stop : Boolean = false
+    private var startTime1: Long = 0
+    private var inferenceTime1 : Long = 0
+    private var firstFrame1 : Boolean = true
+    private var count2: Long = 0
+    private var startTime2: Long = 0
+    private var inferenceTime2 : Long = 0
+    private var firstFrame2 : Boolean = true
+
+    private lateinit var tfliteModel1: MappedByteBuffer
+    private lateinit var tfliteModel2: MappedByteBuffer
+    private var imgData1: ByteBuffer? = null
+    private var imgData2: ByteBuffer? = null
+    private val intValues = IntArray(224 * 224)
+    private val IMAGE_MEAN1 = 128.0f
+    private val IMAGE_STD1 = 128.0f
+
+    private fun convertBitmapToByteBuffer1(bitmap: Bitmap) {
+        if (imgData1 == null) {
+            imgData1 = ByteBuffer.allocateDirect(
+                    1 * 224 * 224 * 3 * 4)
+            imgData1!!.order(ByteOrder.nativeOrder())
+        }
+        imgData1!!.rewind()
         bitmap.getPixels(intValues, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
 
         var pixel = 0
         for (i in 0 until 224) {
             for (j in 0 until 224) {
                 val v: Int = intValues.get(pixel++)
-                imgData!!.putFloat(((v shr 16 and 0xFF) - IMAGE_MEAN) / IMAGE_STD)
-                imgData!!.putFloat(((v shr 8 and 0xFF) - IMAGE_MEAN) / IMAGE_STD)
-                imgData!!.putFloat(((v and 0xFF) - IMAGE_MEAN) / IMAGE_STD)
+                imgData1!!.putFloat(((v shr 16 and 0xFF) - IMAGE_MEAN1) / IMAGE_STD1)
+                imgData1!!.putFloat(((v shr 8 and 0xFF) - IMAGE_MEAN1) / IMAGE_STD1)
+                imgData1!!.putFloat(((v and 0xFF) - IMAGE_MEAN1) / IMAGE_STD1)
+            }
+        }
+    }
+
+    private fun convertBitmapToByteBuffer2(bitmap: Bitmap) {
+        if (imgData2 == null) {
+            imgData2 = ByteBuffer.allocateDirect(
+                1 * 224 * 224 * 3)
+            imgData2!!.order(ByteOrder.nativeOrder())
+        }
+        imgData2!!.rewind()
+        bitmap.getPixels(intValues, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+
+        var pixel = 0
+        for (i in 0 until 224) {
+            for (j in 0 until 224) {
+                val v: Int = intValues.get(pixel++)
+                imgData2!!.put((v shr 16 and 0xFF).toByte())
+                imgData2!!.put((v shr 8 and 0xFF).toByte())
+                imgData2!!.put((v and 0xFF).toByte())
             }
         }
     }
 
     private val RESULTS_TO_SHOW = 3
 
-    private val sortedLabels = PriorityQueue<Map.Entry<String, Float>>(RESULTS_TO_SHOW)
+    private val sortedLabels1 = PriorityQueue<Map.Entry<String, Float>>(RESULTS_TO_SHOW)
         { o1, o2 -> o1.value.compareTo(o2.value) }
+    private val sortedLabels2 = PriorityQueue<Map.Entry<String, Byte>>(RESULTS_TO_SHOW)
+    { o1, o2 -> o1.value.compareTo(o2.value) }
 
-    private lateinit var tflite: Interpreter
+    private lateinit var tflite1: Interpreter
+    private lateinit var tflite2: Interpreter
     private var labelList = ArrayList<String>()
-    private lateinit var outputs: Array<FloatArray>
+    private lateinit var outputs1: Array<FloatArray>
+    private lateinit var outputs2: Array<ByteArray>
 
     private fun loadLabels() {
         val reader = BufferedReader(InputStreamReader(assets.open("labels.txt")))
@@ -152,21 +254,28 @@ class MainActivity: Activity() {
             labelList.add(line)
             line = reader.readLine()
         }
-        outputs = Array(1) { FloatArray(labelList.size) }
+        outputs1 = Array(1) { FloatArray(labelList.size) }
+        outputs2 = Array(1) { ByteArray(labelList.size) }
     }
 
     private fun getNumLabels(): Int {
         return labelList.size
     }
 
-    private fun getBitmap(imageName: String): Bitmap {
+    private fun getBitmap1(imageName: String): Bitmap {
         val stream = BitmapFactory.decodeStream(assets.open(imageName))
         runOnUiThread {
-            imageView.setImageBitmap(Bitmap.createScaledBitmap(stream, 480, 480, true))
+            imageView1.setImageBitmap(Bitmap.createScaledBitmap(stream, 480, 480, true))
         }
         return Bitmap.createScaledBitmap(stream, 224, 224, true)
     }
 
-    fun onNNApiClick(v: View) {
+    private fun getBitmap2(imageName: String): Bitmap {
+        val stream = BitmapFactory.decodeStream(assets.open(imageName))
+        if ((count2 % 8) == 0L) {
+        runOnUiThread {
+            imageView2.setImageBitmap(Bitmap.createScaledBitmap(stream, 480, 480, true))
+        }}
+        return Bitmap.createScaledBitmap(stream, 224, 224, true)
     }
 }
